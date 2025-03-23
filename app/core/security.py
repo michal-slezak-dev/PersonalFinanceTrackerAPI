39eae2e0188a182f204d8730c3a82b1b81d5b42f
Michał Ślęzak <realslezak.michal@gmail.com>
2025-03-23 16:49:43 +0100

    Add JWT create token and verification

--- app/core/security.py
+++ app/core/security.py
@@ -1,11 +1,37 @@
 import bcrypt
+import os
+from dotenv import load_dotenv
+import jwt
+from datetime import datetime, timedelta
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 def get_password_hash(password: str) -> str:
     salt = bcrypt.gensalt()
 
     hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
 
-    return hashed_password
+    return str(hashed_password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(plain_password.encode("utf-8"), hashed_password.encode("utf-8"))
+
+def create_access_token(data: dict, expires_delta: timedelta=timedelta(hours=1)) -> str:
+    data_to_encode = data.copy()
+    expire = datetime.now() + expires_delta
+    data_to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(data_to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
+
+def verify_token(token: str) -> dict:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except jwt.PyJWTError:
+        raise jwt.PyJWTError
