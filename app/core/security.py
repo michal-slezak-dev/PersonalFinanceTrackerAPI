230a76cb26c2bccfddd72a98331f8309d4bf8c05
Michał Ślęzak <realslezak.michal@gmail.com>
2025-03-25 22:02:59 +0100

    Fix password hash

--- app/core/security.py
+++ app/core/security.py
@@ -15,7 +15,7 @@ def get_password_hash(password: str) -> str:
 
     hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
 
-    return str(hashed_password)
+    return hashed_password.decode("utf-8")
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(plain_password.encode("utf-8"), hashed_password.encode("utf-8"))
