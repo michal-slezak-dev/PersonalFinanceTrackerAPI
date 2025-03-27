67f7074be2e836c9bff38b7d266e83d6168208fd
Michał Ślęzak <realslezak.michal@gmail.com>
2025-03-27 22:33:31 +0100

    Add list all categories endpoint & fix create categories endpoint

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from . import models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from sqlalchemy import func
-from .crud import create_category, create_user, update_user, delete_user
+from .crud import create_category, create_user, update_user, delete_user, get_all_categories
 from .models import User
 from .schemas import CategoryCreate, UserResponse, UserCreate, UserUpdate, Token
 from .core.security import verify_password, create_access_token, verify_token, SECRET_KEY, ALGORITHM
@@ -25,8 +25,6 @@ def get_db():
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/v1/auth/token")
 db_dependency = Annotated[Session, Depends(get_db)]
 
-app = FastAPI()
-models.Base.metadata.create_all(bind=engine)
 
 def get_current_user(db: db_dependency, token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
@@ -49,19 +47,14 @@ def get_current_user(db: db_dependency, token: str = Depends(oauth2_scheme)):
 
     return user
 
-
-@app.get("/")
-async def get_authenticated_user_info(db: db_dependency, current_user: User = Depends(get_current_user)):
-    if not current_user:
-        raise HTTPException(
-            status_code=HTTP_401_UNAUTHORIZED,
-            detail="Authentication failed"
-        )
-
-    return {"user": current_user}
+user_dependency = Annotated[User, Depends(get_current_user)]
 
 
-@app.post("/v1/register", response_model=UserResponse)
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+
+@app.post("/v1/auth/register", response_model=UserResponse)
 async def register_user(user_data: UserCreate, db: db_dependency):
     existing_user = db.query(User).filter(User.username == user_data.username).first()
 
@@ -97,8 +90,19 @@ async def login_for_access_token(db: db_dependency, form_data: OAuth2PasswordReq
     return {"access_token": access_token, "token_type": "bearer"}
 
 
+@app.get("/v1/users/me")
+async def get_authenticated_user_info(db: db_dependency, current_user: user_dependency):
+    if not current_user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Authentication failed"
+        )
+
+    return {"user": current_user}
+
+
 @app.patch("/v1/users/me", response_model=UserResponse)
-async def update_user_data(user_update: UserUpdate, db: db_dependency, current_user: User = Depends(get_current_user)):
+async def update_user_data(user_update: UserUpdate, db: db_dependency, current_user: user_dependency):
     user = db.query(User).filter(User.id == current_user.id).first()
     if not user:
         raise HTTPException(
@@ -120,7 +124,7 @@ async def update_user_data(user_update: UserUpdate, db: db_dependency, current_u
     return update_user(db, current_user.id, user_update_data)
 
 @app.delete("/v1/users/me", response_model=UserResponse)
-async def remove_user(db: db_dependency, current_user: User = Depends(get_current_user)):
+async def remove_user(db: db_dependency, current_user: user_dependency):
     user = db.query(User).filter(User.id == current_user.id).first()
     if not user:
         raise HTTPException(
@@ -130,6 +134,17 @@ async def remove_user(db: db_dependency, current_user: User = Depends(get_curren
 
     return delete_user(db, current_user.id)
 
+@app.get("/v1/categories")
+async def list_all_categories(db: db_dependency):
+    return get_all_categories(db)
+
+
 @app.post("/v1/categories")
-async def create_categories(category_data: CategoryCreate, db: db_dependency):
-        return create_category(db, category_data)
+async def create_categories(category_data: CategoryCreate, db: db_dependency, current_user: user_dependency):
+    """Only admins can create new categories (for now)"""
+    if not current_user.is_admin:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="You are not authorized to create categories"
+        )
+    return create_category(db, category_data)
